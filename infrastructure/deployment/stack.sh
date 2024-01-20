7e3879308259c74f096745461c7ecb96810136ab
Usman Mutawakil <umutawakil@Usmans-MacBook-Pro.local>
2024-01-19 23:02:03 -0500

    Added http to https redirect support in cloudfornt distribution

--- infrastructure/deployment/stack.sh
+++ infrastructure/deployment/stack.sh
@@ -6,6 +6,6 @@ command=$1
 stackName=$2
 environment=$3
 
-#echo "Directory: $projectDirectory"
+echo "Directory: $projectDirectory"
 
 aws cloudformation ${command} --stack-name ${stackName}-${environment} --template-body file://${projectDirectory}/infrastructure/stacks/${stackName}/cloudformation/stack.json --parameters file://${projectDirectory}/infrastructure/stacks/${stackName}/cloudformation/environments/${environment}.json --capabilities CAPABILITY_NAMED_IAM
